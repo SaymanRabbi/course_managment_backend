642dd264e3505e0f955ed2cb838672a7cc61b595
sayman rabbi <mdrabbi.asm@gmail.com>
2024-01-06 09:55:12 +0600

    write user info

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,8 +1,19 @@
 const { userCreateController,userLoginController } = require('../Controllers/userController');
 
 const router = require('express').Router();
-
+/**
+ * @postAPI to register user in the application
+ * @steps 1- send the request to the server 
+ * @example http://localhost:5000/api/v1/user/register 
+ * @rule 1- the request must be post request with the body of the request contain the following
+ */
 router.post('/register', userCreateController);
+/**
+ * @getAPI to login user in the application
+ * @steps 1- send the request to the server 
+ * @example http://localhost:5000/api/v1/user/login 
+ * @rule 1- the request must be get request with the body of the request contain the following
+ */
 router.get('/login',userLoginController)
 
 module.exports = router;
