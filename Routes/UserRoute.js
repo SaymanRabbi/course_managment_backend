311acef2835f4e18be9a4c0ea75ef6bf825bc2d1
sayman rabbi <mdrabbi.asm@gmail.com>
2024-01-09 09:12:49 +0600

    i will back to you soon

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,37 +1,46 @@
-const { userCreateController,userLoginController,updateUserController } = require('../Controllers/userController');
-const { apiLimiter } = require('../Middlewares/ApiLimiter');
-const { authorization } = require('../Middlewares/IsAdmin');
-const VerifyToken = require('../Middlewares/VerifyToken');
-const { VerifyTokenServices } = require('../Services/userServices');
-
-const router = require('express').Router();
+const {
+  userCreateController,
+  userLoginController,
+  updateUserController,
+} = require("../Controllers/userController");
+const { apiLimiter } = require("../Middlewares/ApiLimiter");
+const { authorization } = require("../Middlewares/IsAdmin");
+const VerifyToken = require("../Middlewares/VerifyToken");
+const { VerifyTokenServices } = require("../Services/userServices");
+const router = require("express").Router();
 /**
  * @postAPI to register user in the application
- * @steps 1- send the request to the server 
- * @example http://localhost:5000/api/v1/user/register 
+ * @steps 1- send the request to the server
+ * @example http://localhost:5000/api/v1/user/register
  * @rule 1- the request must be post request with the body of the request contain the following
  */
-router.post('/register', apiLimiter,userCreateController);
+router.post("/register", apiLimiter, userCreateController);
 /**
  * @getAPI to login user in the application
- * @steps 1- send the request to the server 
- * @example http://localhost:5000/api/v1/user/login 
+ * @steps 1- send the request to the server
+ * @example http://localhost:5000/api/v1/user/login
  * @rule 1- the request must be get request with the body of the request contain the following
  */
-router.get('/login',apiLimiter,userLoginController)
+router.get("/login", apiLimiter, userLoginController);
 /**
- * @updateAPI to update user role 
+ * @updateAPI to update user role
  * @steps 1- send the request to the server
- * @example http://localhost:5000/api/v1/user/updateRole 
+ * @example http://localhost:5000/api/v1/user/updateRole
  * @rule 1- the request must be put request with the id of the user in the params and the body of the request contain the following
  */
-router.put('/update/:id',apiLimiter,VerifyToken,authorization('admin',"teacher"),updateUserController)
+router.put(
+  "/update/:id",
+  apiLimiter,
+  VerifyToken,
+  authorization("admin", "teacher"),
+  updateUserController
+);
 /**
- * @updateAPI to update user verification 
+ * @updateAPI to update user verification
  * @steps 1- send the request to the server
- * @example http://localhost:5000/api/v1/user/verify-email 
+ * @example http://localhost:5000/api/v1/user/verify-email
  * @rule 1- the request must be put request with the token of the user in the params
  */
-router.put('/verify-email/:token',VerifyTokenServices)
+router.put("/verify-email/:token", VerifyTokenServices);
 
 module.exports = router;
--- Services/userServices.js
+++ Services/userServices.js
@@ -1,68 +1,85 @@
 const UserModel = require("../Models/UserModel");
 const sendMail = require("../utils/sendMail");
-const jwt = require('jsonwebtoken');
-const {promisify} = require('util');
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
-exports.userCreateServices= async (user) => {
-    try {
-        const userCreated = (await UserModel.create(user)).save();
-        return userCreated;
-    } catch (error) {
-        throw error;
-    }
-}
+exports.userCreateServices = async (user) => {
+  try {
+    const userCreated = (await UserModel.create(user)).save();
+    return userCreated;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // -----get userLoginServices------
-exports.userLoginServices = async (email)=>{
-    try {
-        const getUser = await UserModel.findOne({
-            email
-        })
-        return getUser
-    } catch (error) {
-        throw error
-    }
-}
+exports.userLoginServices = async (email) => {
+  try {
+    const getUser = await UserModel.findOne({
+      email,
+    });
+    return getUser;
+  } catch (error) {
+    throw error;
+  }
+};
 // -----get userLoginServices------
 // -----update user role------
-exports.updateUserServices = async (id,role)=>{
-    try {
-        const oldUser = await UserModel.findById(id);
-        const updateUser = await UserModel.findByIdAndUpdate(id, { role }, { new: true, useFindAndModify: false });
-        if (updateUser) {
-            // Check if the role was actually updated
-            if (oldUser.role !== updateUser.role) {
-                await sendMail(updateUser);
-                return updateUser
-            } else {
-               return null
-            }
-        } else {
-            return null
-        }
-        
-    } catch (error) {
-        throw error
+exports.updateUserServices = async (id, role) => {
+  try {
+    const oldUser = await UserModel.findById(id);
+    const updateUser = await UserModel.findByIdAndUpdate(
+      id,
+      { role },
+      { new: true, useFindAndModify: false }
+    );
+    if (updateUser) {
+      // Check if the role was actually updated
+      if (oldUser.role !== updateUser.role) {
+        await sendMail(updateUser);
+        return updateUser;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
     }
-}
-exports.VerifyTokenServices = async (req,res)=>{
-    try {
-        const {token} = req.params;
-        const user =await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-        const check = await UserModel.findById(user._id);
-        if(check.isVerified ===true ){
-          return   res.status(400).send({
-                status:false,
-                message:"User already verified"
-             });
-        } else{
-            await UserModel.findByIdAndUpdate(user._id,{isVerified:true});
-           return res.status(200).send({
-                status:true,
-                message:"User verified successfully"
-            });
+  } catch (error) {
+    throw error;
+  }
+};
+exports.VerifyTokenServices = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const user = await promisify(jwt.verify)(
+      token,
+      process.env.JWT_SECRET,
+      (err, decoded) => {
+        if (err) {
+          return res.status(400).send({
+            status: false,
+            message: "Token Expired",
+          });
         }
-    } catch (error) {
-        throw error
+      }
+    );
+    const check = await UserModel.findById(user?._id);
+    if (check.isVerified === true) {
+      return res.status(400).send({
+        status: false,
+        message: "User already verified",
+      });
+    } else {
+      const data = await UserModel.findByIdAndUpdate(user._id, {
+        isVerified: true,
+      });
+      return res.status(200).send({
+        status: true,
+        message: "User verified successfully",
+        data,
+      });
     }
-}
+  } catch (error) {
+    throw error;
+  }
+};
